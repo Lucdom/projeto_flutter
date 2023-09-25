973606373f5c1ff2ae327bdc91242ef7e22e5524
unknown <jeremyvdvinne@gmail.com>
2023-09-25 17:51:08 -0300

    add TODO list to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_flutter/myApp.dart';
 
+/* TO DO
+snake case
+cadastro de infos
+validação
+login com banco
+provider
+*/
+
 void main() {
   runApp(const MyApp());
 }
